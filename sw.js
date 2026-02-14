8c83a5c2f81f01cda97aa8c6947034ee37c35e21
Sleepy Fox <sleepyfox33284@gmail.com>
2026-02-14 18:57:27 +0200

    modified sw.js, manifest.json, index.html

--- sw.js
+++ sw.js
@@ -19,13 +19,13 @@ self.addEventListener('fetch', event => {
 //   './icons/icon-512.png',
 // ]
 
-// // Встановлення: зберігаємо файли в кеш
+
 // self.addEventListener('install', event => {
 //   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)))
 //   self.skipWaiting()
 // })
 
-// // Робота з мережею: спочатку беремо з кешу, якщо немає — з мережі
+
 // self.addEventListener('fetch', event => {
 //   event.respondWith(
 //     caches.match(event.request).then(response => {
